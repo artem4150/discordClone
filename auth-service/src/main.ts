30ef6b1abf2a62c20bc55af72b88011d8971ddd9
artem4150 <52111050+artem4150@users.noreply.github.com>
2025-07-01 19:41:31 +0500

    Enable CORS for auth-service

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const origins = process.env.ALLOW_ORIGINS
+    ? process.env.ALLOW_ORIGINS.split(',')
+    : true;
+
+  app.enableCors({ origin: origins, credentials: true });
   
   app.setGlobalPrefix('');  // Kong проксирует «/auth»
   app.useGlobalPipes(new ValidationPipe());
